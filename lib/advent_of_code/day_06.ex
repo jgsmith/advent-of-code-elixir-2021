157f5db0cc253ee72c2961cbe824e533f4774821
James Smith <jgsmith@gmail.com>
2021-12-11 10:15:03 -0500

    Day 06

--- lib/advent_of_code/day_06.ex
+++ lib/advent_of_code/day_06.ex
@@ -1,7 +1,41 @@
 defmodule AdventOfCode.Day06 do
   def part1(args) do
+    args
+    |> parse_input()
+    |> ticks(80)
+    |> Map.values()
+    |> Enum.sum()
   end
 
   def part2(args) do
+    args
+    |> parse_input()
+    |> ticks(256)
+    |> Map.values()
+    |> Enum.sum()
   end
+
+  def parse_input(source) do
+    source
+    |> String.split(",", trim: true)
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.group_by(fn x -> x end)
+    |> Enum.map(fn {k, vs} -> {k, Enum.count(vs)} end)
+    |> Enum.into(%{})
+  end
+
+  def ticks(ages, 0), do: ages
+  def ticks(ages, n), do: ticks(tick(ages), n - 1)
+
+  def tick(ages) do
+    {new_fish, new_ages} = Enum.reduce(ages, {0, %{}}, &age_fish/2)
+
+    Map.put(new_ages, 8, new_fish)
+  end
+
+  def age_fish({0, count}, {new_fish, new_ages}),
+    do: {new_fish + count, Map.update(new_ages, 6, count, fn old_count -> old_count + count end)}
+
+  def age_fish({n, count}, {new_fish, new_ages}),
+    do: {new_fish, Map.update(new_ages, n - 1, count, fn old_count -> old_count + count end)}
 end
