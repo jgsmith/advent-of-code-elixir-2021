f61e10b482a07bb88288c9789e2e290b87a428b0
James Smith <jgsmith@gmail.com>
2021-12-25 13:30:52 -0500

    Day 18

--- lib/advent_of_code/day_18.ex
+++ lib/advent_of_code/day_18.ex
@@ -1,7 +1,159 @@
 defmodule AdventOfCode.Day18 do
   def part1(args) do
+    args
+    |> parse_input()
+    |> sum()
+    |> magnitude()
   end
 
   def part2(args) do
+    numbers = parse_input(args)
+
+    numbers
+    |> Enum.flat_map(fn n ->
+      Enum.map(numbers, fn m ->
+        if n != m, do: magnitude(sum([m, n])), else: 0
+      end)
+    end)
+    |> Enum.max()
   end
+
+  def parse_input(source) do
+    source
+    |> String.trim()
+    |> String.split(~r{\s*\n\s*}, trim: true)
+    |> Enum.map(&parse_line/1)
+  end
+
+  @spec parse_line(binary, list) :: [integer | :up | :down | list]
+  def parse_line(line, acc \\ [])
+  # [\d+,\d+] => [m, n]
+  # \d => m
+  # [ => :down
+  # ] => :up
+  def parse_line("", acc), do: Enum.reverse(acc)
+  def parse_line(<<"[", rest::binary>>, acc), do: parse_line(rest, [:down | acc])
+  def parse_line(<<"]", rest::binary>>, acc), do: parse_line(rest, [:up | acc])
+  def parse_line(<<",", rest::binary>>, acc), do: parse_line(rest, acc)
+
+  def parse_line(line, acc) do
+    [_, number, rest] = Regex.run(~r{^(\d+),?(.*)$}, line)
+    parse_line(rest, [String.to_integer(number) | acc])
+  end
+
+  @spec snailnumber_to_string([:down | :up | integer], list) :: binary
+  def snailnumber_to_string(number, acc \\ [])
+
+  def snailnumber_to_string([], acc), do: Enum.join(Enum.reverse(acc))
+
+  def snailnumber_to_string([:down, n | rest], acc) when is_integer(n),
+    do: snailnumber_to_string(rest, ["[#{n}," | acc])
+
+  def snailnumber_to_string([:up, :down | rest], acc),
+    do: snailnumber_to_string([:down | rest], ["]," | acc])
+
+  def snailnumber_to_string([n | rest], acc) when is_integer(n),
+    do: snailnumber_to_string(rest, ["#{n}" | acc])
+
+  def snailnumber_to_string([:down | rest], acc), do: snailnumber_to_string(rest, ["[" | acc])
+
+  def snailnumber_to_string([:up, n | rest], acc) when is_integer(n),
+    do: snailnumber_to_string(rest, ["],#{n}" | acc])
+
+  def snailnumber_to_string([:up | rest], acc), do: snailnumber_to_string(rest, ["]" | acc])
+
+  def add(m, n), do: [:down] ++ m ++ n ++ [:up]
+
+  def inspect_snail(sn) do
+    IO.inspect(snailnumber_to_string(sn))
+    sn
+  end
+
+  def sum([first]), do: first
+
+  def sum([first, second | snail_numbers]) do
+    sum([reduce(add(first, second)) | snail_numbers])
+  end
+
+  # [:down, 4, :down, 17, 5, :up, :up]
+  def magnitude(n, acc \\ [])
+  def magnitude([n], []) when is_integer(n), do: n
+
+  def magnitude([], acc), do: magnitude(Enum.reverse(acc))
+
+  def magnitude([:down, m, n, :up | rest], acc) when is_integer(m) and is_integer(n) do
+    magnitude(rest, [3 * m + 2 * n | acc])
+  end
+
+  def magnitude([bracket, m | [bracket | _] = rest], acc)
+      when is_integer(m) and bracket in [:up, :down],
+      do: magnitude(rest, [m, bracket | acc])
+
+  def magnitude([el | rest], acc), do: magnitude(rest, [el | acc])
+
+  def reduce(n) do
+    case try_exploding(n) do
+      false ->
+        case try_splitting(n) do
+          false -> n
+          new_n when is_list(new_n) -> reduce(new_n)
+        end
+
+      new_n when is_list(new_n) ->
+        reduce(new_n)
+    end
+  end
+
+  def try_exploding(n, depth \\ 0, acc \\ [])
+  def try_exploding([], _, _), do: false
+
+  def try_exploding([:down, m, n, :up | rest], depth, acc)
+      when is_integer(m) and is_integer(n) and depth == 4 do
+    # split up m and n
+    # end with an array
+    # m goes into acc, and n goes into rest
+    Enum.reverse(add_to_first_regular_number(acc, m)) ++
+      [0] ++ add_to_first_regular_number(rest, n)
+  end
+
+  def try_exploding([:down | rest], depth, acc), do: try_exploding(rest, depth + 1, [:down | acc])
+  def try_exploding([:up | rest], depth, acc), do: try_exploding(rest, depth - 1, [:up | acc])
+  def try_exploding([n | rest], depth, acc), do: try_exploding(rest, depth, [n | acc])
+
+  def add_to_first_regular_number(list, n, acc \\ [])
+  def add_to_first_regular_number([], _, acc), do: Enum.reverse(acc)
+
+  def add_to_first_regular_number([a | rest], n, acc) when is_integer(a) do
+    Enum.reverse(acc) ++ [a + n | rest]
+  end
+
+  # def add_to_first_regular_number([:down, a, b, :up | rest], n, acc)
+  #     when is_integer(a) and is_integer(b) do
+  #   add_to_first_regular_number(rest, n, [:up, b, a, :down | acc])
+  # end
+
+  # def add_to_first_regular_number([m, bracket | rest], n, acc)
+  #     when is_integer(m) and bracket in [:up, :down] do
+  #   Enum.reverse(acc) ++ [m + n, bracket | rest]
+  # end
+
+  # def add_to_first_regular_number([bracket, m, bracket | rest], n, acc)
+  #     when is_integer(m) and is_integer(n) and bracket in [:up, :down] do
+  #   Enum.reverse(acc) ++ [bracket, m + n, bracket | rest]
+  # end
+
+  def add_to_first_regular_number([el | rest], n, acc),
+    do: add_to_first_regular_number(rest, n, [el | acc])
+
+  def try_splitting(n, acc \\ [])
+
+  def try_splitting([], _), do: false
+
+  def try_splitting([n | rest], acc)
+      when is_integer(n) and n >= 10 do
+    left = div(n, 2)
+    Enum.reverse(acc) ++ [:down, left, n - left, :up | rest]
+  end
+
+  def try_splitting([el | rest], acc), do: try_splitting(rest, [el | acc])
 end
--- test/advent_of_code/day_18_test.exs
+++ test/advent_of_code/day_18_test.exs
@@ -3,12 +3,86 @@ defmodule AdventOfCode.Day18Test do
 
   import AdventOfCode.Day18
 
-  @tag :skip
   test "part1" do
-    input = nil
+    input = """
+    [[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]
+    [[[5,[2,8]],4],[5,[[9,9],0]]]
+    [6,[[[6,2],[5,6]],[[7,6],[4,7]]]]
+    [[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]
+    [[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]
+    [[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]
+    [[[[5,4],[7,7]],8],[[8,3],8]]
+    [[9,3],[[9,9],[6,[4,9]]]]
+    [[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]
+    [[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]
+    """
+
     result = part1(input)
 
-    assert result
+    assert result == 4140
+  end
+
+  test "sum" do
+    assert snailnumber_to_string(
+             sum([
+               parse_line("[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]"),
+               parse_line("[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]")
+             ])
+           ) == "[[[[4,0],[5,4]],[[7,7],[6,0]]],[[8,[7,7]],[[7,9],[5,0]]]]"
+
+    assert snailnumber_to_string(
+             sum([parse_line("[[[[4,3],4],4],[7,[[8,4],9]]]"), parse_line("[1,1]")])
+           ) == "[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]"
+  end
+
+  test "magnitude" do
+    assert magnitude(parse_line("[17,5]")) == 17 * 3 + 5 * 2
+    assert magnitude(parse_line("[4,[17,5]]")) == 4 * 3 + (17 * 3 + 5 * 2) * 2
+    assert magnitude(parse_line("[[1,2],[[3,4],5]]")) == 143
+  end
+
+  test "try_exploding" do
+    assert try_exploding(parse_line("[[[[[9,8],1],2],3],4]")) == parse_line("[[[[0,9],2],3],4]")
+    assert try_exploding(parse_line("[7,[6,[5,[4,[3,2]]]]]")) == parse_line("[7,[6,[5,[7,0]]]]")
+    assert try_exploding(parse_line("[[6,[5,[4,[3,2]]]],1]")) == parse_line("[[6,[5,[7,0]]],3]")
+
+    assert try_exploding(parse_line("[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]")) ==
+             parse_line("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]")
+
+    assert try_exploding(parse_line("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]")) ==
+             parse_line("[[3,[2,[8,0]]],[9,[5,[7,0]]]]")
+
+    assert snailnumber_to_string(
+             try_exploding(parse_line("[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]"))
+           ) == "[[[[0,7],4],[7,[[8,4],9]]],[1,1]]"
+
+    assert snailnumber_to_string(try_exploding(parse_line("[[[[0,7],4],[7,[[8,4],9]]],[1,1]]"))) ==
+             "[[[[0,7],4],[15,[0,13]]],[1,1]]"
+
+    assert snailnumber_to_string(
+             try_exploding(parse_line("[[[[0,7],4],[[7,8],[0,[6,7]]]],[1,1]]"))
+           ) ==
+             "[[[[0,7],4],[[7,8],[6,0]]],[8,1]]"
+  end
+
+  test "try_splitting" do
+    assert try_splitting(parse_line("[[[[0,7],4],[15,[0,13]]],[1,1]]")) ==
+             parse_line("[[[[0,7],4],[[7,8],[0,13]]],[1,1]]")
+
+    assert try_splitting(parse_line("[[[[0,7],4],[[7,8],[0,13]]],[1,1]]")) ==
+             parse_line("[[[[0,7],4],[[7,8],[0,[6,7]]]],[1,1]]")
+
+    assert snailnumber_to_string(try_splitting(parse_line("[[[[0,7],4],[15,[0,13]]],[1,1]]"))) ==
+             "[[[[0,7],4],[[7,8],[0,13]]],[1,1]]"
+
+    assert snailnumber_to_string(try_splitting(parse_line("[[[[0,7],4],[[7,8],[0,13]]],[1,1]]"))) ==
+             "[[[[0,7],4],[[7,8],[0,[6,7]]]],[1,1]]"
+  end
+
+  test "reduce" do
+    assert snailnumber_to_string(
+             reduce(sum([parse_line("[[[[4,3],4],[7,[[8,4],9]]]"), parse_line("[1,1]")]))
+           ) == "[[[[0,7],4],[[7,8],[6,0]]],[8,1]]"
   end
 
   @tag :skip
